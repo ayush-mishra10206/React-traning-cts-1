735fc629047e3ad041008a983a599480b94d7cfd
unknown <cogjava1774@iiht.tech>
2022-07-06 10:31:53 +0530

    added state in function

--- React/app1/src/App.js
+++ React/app1/src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import OrderForm from './orderform';
 
-function App() {
+function App() { // constructor function 
   return (
     <div className="App">
       <header className="App-header">
        <OrderForm name="abc" ></OrderForm>
+       <OrderForm name="bcd" ></OrderForm>
        <OrderForm name="bcd" ></OrderForm>
       </header>
     </div>
--- React/app1/src/orderform.js
+++ React/app1/src/orderform.js
@@ -1,11 +1,22 @@
+import { useState } from "react"
 
 
 
 export default function OrderForm(props) {
+    const [order, setOrder]= useState({
+        item:'laptop'
+    })
+    function updateValue(event){
+        console.log(event.target.value)
+        setOrder({item:event.target.value})
+    }
     return (
         <div>
             <h1>Order form</h1>
-            <input value={props.name}></input>
+            <input value={order.item} onChange={updateValue}></input>
+            <div>
+            <h1>{props.name}</h1>
+            </div>
         </div>
     )
 }
