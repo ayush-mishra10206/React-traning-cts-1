c6d6cea1c6c66013cff1c7cb065c8d3141ee27f6
unknown <cogjava1774@iiht.tech>
2022-07-07 13:43:05 +0530

    added folder day4 and adding css in orderFrom

--- React/app1/src/orderform.js
+++ React/app1/src/orderform.js
@@ -22,18 +22,45 @@ export default function OrderForm(props) {
         const first = [7, 3, 12];
         const second = [89, 5, 90];
         return [...first, ...second];
-        
+
     }
     return (
-        <div>
-            <h1>Order form</h1>
-            <input value={order.item} name='item' onChange={updateValue}></input>
-            <input value={order.price} min="0" type="number" name='price' onChange={updateValue}></input>
-            <input value={order.tax} min="0" type="number" name="tax" onChange={updateValue}></input>
-            Total:{calculate()}
+        <div className="container">
+            <label className="header">Order form</label>
+            <div className="row">
+                <div className="col-25">
+                    <label >item</label>
+                </div>
+                <div className="col-75">
+                    <input value={order.item} id="text" type="text" name='item' onChange={updateValue} />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-25">
+                    <label >price</label>
+                </div>
+                <div className="col-75">
+                    <input value={order.price} id="price" type="text" name='price' onChange={updateValue} />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-25">
+                    <label >tax</label>
+                </div>
+                <div className="col-75">
+                    <input value={order.tax} id="tex" min="0" max="100" type="range" name='tax' onChange={updateValue} />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-25">
+                    <label >Total</label>
+                </div>
+                <div className="col-75">
+                    {calculate()}
+                </div>
+            </div>
             <div>
             </div>
-            {checkingSpreadOperator()}
         </div>
     )
 }
