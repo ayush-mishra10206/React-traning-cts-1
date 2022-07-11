34bb05ab3f2834a6aa9dea1087177dbe518443ee
unknown <cogjava1774@iiht.tech>
2022-07-11 12:05:34 +0530

    created a new project form-validation

---
+++ React/form-validation/src/formvalidation.js
@@ -0,0 +1,56 @@
+import { useState } from "react"
+import './formvalidation.css'
+
+export default function FormValidation(props) {
+
+    const [userData, setUserData] = useState({
+        emailId: '',
+        passWord: '',
+        confirmPassword: ''
+    })
+
+    function onChangeUserData(event) {
+        setUserData({
+            ...userData, [event.target.name]: event.target.value
+        })
+    }
+
+    return (
+        <div className="outerContainer">
+            <div className="header">
+                USER FORM
+            </div>
+            <div className="row">
+                <div className="col-25">
+                    <label> Email ID :</label>
+                </div>
+                <div className="col-75">
+                    <input className='inputField' value={userData.emailId} name="emailId" onChange={onChangeUserData} ></input>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-25">
+                    <label>Password :</label>
+                </div>
+                <div className="col-75">
+                    <input className='inputField' value={userData.passWord} type="password" name="passWord" onChange={onChangeUserData}></input>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-25">
+                    <label> Confirm Password :</label>
+                </div>
+                <div className="col-75">
+                    <input className='inputField' value={userData.confirmPassword} type="password" name="confirmPassword" onChange={onChangeUserData}></input>
+                </div>
+            </div>
+            {
+                userData.passWord !== userData.confirmPassword ? <div className='errorMessage'>
+                    Password and confirm password did not matched
+                </div>
+                    : null
+            }
+
+        </div>
+    )
+}
